e07eab43cc932ac43030927da95dc8541faa32ec
HANG <wangxiao_h@126.com>
2021-07-03 15:03:49 +0800

    log与分页

--- mybatis_03/src/test/java/UserTest.java
+++ mybatis_03/src/test/java/UserTest.java
@@ -1,15 +1,20 @@
 import com.yaozhou.dao.UserMapper;
 import com.yaozhou.pojo.User;
-import org.apache.ibatis.session.SqlSession;
 import com.yaozhou.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
+
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by WXHang on HANG at 2021/6/30 0:45
  */
 public class UserTest {
+    static Logger logger = Logger.getLogger(UserTest.class);
     @Test
     public void Test1(){
         //获取sqlsession对象
@@ -21,7 +26,7 @@ public class UserTest {
         //List<User> userList = sqlSession.selectList("com.yaozhou.dao.UserDao.getUserList");
         List<User> userList = userDao.getUserList();
         for (User user : userList) {
-            System.out.println(user.getUserName());
+            System.out.println(user);
         }
         //关闭sqlsession流
         sqlSession.close();
@@ -86,6 +91,27 @@ public class UserTest {
         //方法2：
         //List<User> userList = sqlSession.selectList("com.yaozhou.dao.UserDao.getUserList");
         List<User> userList = mapper.getUserListLike("孙");
+        for (User user : userList) {
+            System.out.println(user.getUserName());
+        }
+
+        //关闭sqlsession流
+        sqlSession.close();
+    }
+    @Test
+    public void getUserListLimit(){
+        //获取sqlsession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //getMapper
+        //方法1：
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //方法2：
+        //List<User> userList = sqlSession.selectList("com.yaozhou.dao.UserDao.getUserList");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("currentPage",2);
+        map.put("pageSize",5);
+
+        List<User> userList = mapper.getUserListLimit(map);
         for (User user : userList) {
             System.out.println(user.getUserName());
         }
